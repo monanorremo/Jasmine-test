ecf41f1108ce3319ac9c344b1b806c40c34b498c
monanorremo <mona.norremo@gmail.com>
2020-09-22 12:43:38 +0000

    Defensive Programming

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,3 +1,10 @@
-function addition(){
-    return 42;
+function addition(numOne, numTwo){
+    if(typeof(numOne) == "number" && typeof(numTwo) == "number"){
+return numOne + numTwo;
+} else {
+    return "Error!";
 }
+    }
+    
+    
+
