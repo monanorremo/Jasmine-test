a261abb59f9f9b3aa8bd6568a1824e9f448aa8d2
monanorremo <mona.norremo@gmail.com>
2020-09-24 14:38:55 +0000

    Setting up each spec

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,8 +1,10 @@
 
 describe("Calculator", function() {
+beforeEach(function() {
+calc = new Calculator;
 
-    var calc = new Calculator;
-
+});
+     
    describe("Addition function", function() {
         it("should return 42", function() {
             calc.add(20);
